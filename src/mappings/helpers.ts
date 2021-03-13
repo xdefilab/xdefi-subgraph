902ff1d06eef03563682031fae348d838a4c56ad
jingleizhang <eric@xdefi.com>
2021-03-13 17:12:49 +0800

    update mainnet usdt address

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -28,8 +28,8 @@ let network = dataSource.network()
 
 // Config for mainnet
 let WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-let USD = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' // USDC
-let DAI = '0x6b175474e89094c44da98b954eedeac495271d0f'
+let USD = '0xdac17f958d2ee523a2206206994597c13d831ec7' //USDT
+let DAI = '0x6b175474e89094c44da98b954eedeac495271d0f' //DAI
 
 if (network == 'kovan') {
     WETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
